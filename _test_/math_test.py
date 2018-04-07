b43007d2f15e0e298d97b29cfd63a2b231081e2a
Schubert David Rodriguez <scrodrig@thoughtworks.com>
2018-04-07 16:12:51 -0500

    Add a couple of tests

--- _test_/math_test.py
+++ _test_/math_test.py
@@ -11,3 +11,11 @@ class TestMathCase(unittest.TestCase):
     def test_odd_number_is_not_none(self):
         number = self.fake.random_number()
         self.assertIsNotNone(OddNumber.odd_number(number))
+
+    def test_return_true_when_odd_number_is_odd(self):
+        number = self.fake.random_number() * 2
+        self.assertEqual(OddNumber.odd_number(number), True)
+
+    def test_return_false_when_odd_number_is_even(self):
+        number = self.fake.random_number() * 2 + 1
+        self.assertEqual(OddNumber.odd_number(number), False)
